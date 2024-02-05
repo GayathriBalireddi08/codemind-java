d20e9b0b7f52ef106681769ce14f169c3baf8ba6
GayathriBalireddi08 <119841081+GayathriBalireddi08@users.noreply.github.com>
2024-02-05 12:01:52 +0530

    import java.util.*; public class Solutions{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int a = sc.nextInt();         int b = sc.nextInt();         int c;         c= a*b;         System.out.printf("%d",c);     } }

---
+++ Instant_Noodles.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Solutions{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c;
+        c= a*b;
+        System.out.printf("%d",c);
+    }
+}
