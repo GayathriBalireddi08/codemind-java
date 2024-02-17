613cf06b471fa9d46a427104ef6d6f5bb2a91589
GayathriBalireddi08 <119841081+GayathriBalireddi08@users.noreply.github.com>
2024-02-17 11:58:35 +0530

    import java.util.*; public class Max{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         if(a>=18)         {             System.out.print("YES");         }         else         {             System.out.print("NO");         }     } }

---
+++ Casting_the_Vote.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Max{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a>=18)
+        {
+            System.out.print("YES");
+        }
+        else
+        {
+            System.out.print("NO");
+        }
+    }
+}
