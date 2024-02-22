31de956779c8b182922551e965213a6aa9a3cd7a
GayathriBalireddi08 <119841081+GayathriBalireddi08@users.noreply.github.com>
2024-02-22 17:10:17 +0530

    import java.util.*; public class Difference{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         int y=sc.nextInt();         int z=y-x;         System.out.printf("%d",z);     } }

---
+++ Donation.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Difference{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=y-x;
+        System.out.printf("%d",z);
+    }
+}
