1818f2e5d53fd55b55592ec129f273a72f1a462f
GayathriBalireddi08 <119841081+GayathriBalireddi08@users.noreply.github.com>
2024-02-22 17:29:03 +0530

    import java.util.*; public class Colors{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         char ch=sc.next().charAt(0);         if(ch=='v')         {             System.out.println("Violet");         }         else if(ch=='I')         {             System.out.println("Indigo");         }         else if(ch=='B')         {             System.out.println("Blue");         }         else if(ch=='G')         {             System.out.println("Green");         }         else if(ch=='Y')         {             System.out.println("Yellow");         }         else if(ch=='O')         {             System.out.println("Orange");         }         else if(ch=='R')         {             System.out.println("Red");         }         else         {             System.out.println(-1);         }     } }

---
+++ Print_the_color_name.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class Colors{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        char ch=sc.next().charAt(0);
+        if(ch=='v')
+        {
+            System.out.println("Violet");
+        }
+        else if(ch=='I')
+        {
+            System.out.println("Indigo");
+        }
+        else if(ch=='B')
+        {
+            System.out.println("Blue");
+        }
+        else if(ch=='G')
+        {
+            System.out.println("Green");
+        }
+        else if(ch=='Y')
+        {
+            System.out.println("Yellow");
+        }
+        else if(ch=='O')
+        {
+            System.out.println("Orange");
+        }
+        else if(ch=='R')
+        {
+            System.out.println("Red");
+        }
+        else
+        {
+            System.out.println(-1);
+        }
+    }
+}
